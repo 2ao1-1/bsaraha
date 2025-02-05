c736dd32793425fa2f63b5b329e4f30c74c01431
Your GitHub Username <Your GitHub Email>
2025-02-05 20:52:43 +0200

    v.1 final edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,19 +13,16 @@ export default {
           main: "#FAFAFA",
           lighter: "#FFFFFF",
           darker: "#F3F4F6",
-          usage: "60% - خلفيات وكروت رئيسية",
         },
         secondary: {
           main: "#4338CA",
           lighter: "#6366F1",
           darker: "#3730A3",
-          usage: "30% - عناصر تفاعلية وأزرار",
         },
         accent: {
           main: "#312E81",
           lighter: "#4338CA",
           darker: "#27284B",
-          usage: "10% - تفاصيل وتأكيدات",
         },
         text: {
           primary: "#111827",
