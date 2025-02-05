d8194862a286afc5c1b5e60d97d1473afe59456a
Your GitHub Username <Your GitHub Email>
2025-02-05 21:09:42 +0200

    v.1 final edit

---
+++ src/pages/components/SecretKey.jsx
@@ -0,0 +1,11 @@
+export const SECRET_KEY = "MySuperSecretKey";
+
+export const POST_NEWUSER = `http://64.23.184.122:2001/api/auth/register`;
+
+export const GET_EXISTUSER = "http://64.23.184.122:2001/api/auth/login";
+
+export const GET_USER_MESSAGES = "http://64.23.184.122:2001/api/messages/";
+
+export const POST_MESSAGES_TOUSER = "http://64.23.184.122:2001/api/messages/";
+
+export const USERS = "http://64.23.184.122:2001/api/users";
