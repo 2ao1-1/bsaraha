a51cd69ad972b078a74eb4631f2792b47d40b8e4
Your GitHub Username <Your GitHub Email>
2025-06-23 16:27:49 +0300

    ll

--- src/components/Apis.jsx
+++ src/components/Apis.jsx
@@ -1,11 +1,11 @@
-const baseUrl = `http://144.91.75.57:2001`;
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const POST_NEWUSER = `${baseUrl}/api/auth/register`;
+export const POST_NEWUSER = `${BASE_URL}/api/auth/register`;
 
-export const GET_EXISTUSER = `${baseUrl}/api/auth/login`;
+export const GET_EXISTUSER = `${BASE_URL}/api/auth/login`;
 
-export const GET_USER_MESSAGES = `${baseUrl}/api/messages/`;
+export const GET_USER_MESSAGES = `${BASE_URL}/api/messages/`;
 
-export const POST_MESSAGES_TOUSER = `${baseUrl}/api/messages/`;
+export const POST_MESSAGES_TOUSER = `${BASE_URL}/api/messages/`;
 
-export const USERS = `${baseUrl}/api/users`;
+export const USERS = `${BASE_URL}/api/users`;
